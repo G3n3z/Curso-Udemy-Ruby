68b986ecd97c09f7ecd55a1a9333ddaf7043b7e9
Daniel Fernandes <danielalmfernandes@hotmail.com>
2022-09-23 15:16:15 +0100

    Add more validations to user

--- Section4/alpha-blog/app/models/article.rb
+++ Section4/alpha-blog/app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  belongs_to :user
   validates :title, length: {minimum: 6, maximum: 100}, presence: true
   validates :description,  presence: true, length: {minimum: 10, maximum: 1000}
 
