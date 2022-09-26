7b5166cc943e4e205077766531abe47140debf94
Daniel Fernandes <danielalmfernandes@hotmail.com>
2022-09-26 15:16:45 +0100

    user login

--- message_me/app/controllers/sessions_controller.rb
+++ message_me/app/controllers/sessions_controller.rb
@@ -4,4 +4,33 @@ class SessionsController < ApplicationController
 
   end
 
+  def create
+    user = User.find_by_username(params[:session][:username])
+    if !user
+      flash.now[:error] = "Username dont exists"
+      render "new"
+    end
+    if user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "you have successfully logged in"
+      redirect_to root_path
+    else
+      flash.now[:error] = "Something is bad with your credentials"
+      render "new"
+    end
+
+  end
+
+  def destroy
+    session[:id] = nil
+    flash[:success] = "you have successfully logged out"
+    redirect_to login_path
+  end
+
+  private
+  def session_params
+    params.require(:session).permit(:username, :password)
+
+  end
+
 end
--- message_me/db/seeds.rb
+++ message_me/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Daniel", password: "asd")
+User.create(username: "Josefina", password: "asd")
+User.create(username: "Raquel", password: "asd")
